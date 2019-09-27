ba97d5b43f54b6decc51195bb83110f548588285
Benjamin Broestl <Bensgamingmail@yahoo.com>
2019-09-27 16:39:11 -0400

    Initial Commit. main.dart, appdelegate.m and MainActivity.java for native battery report

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Native Code',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _batteryLevel;
+  // Establish a connection with native state
+  Future<void> _getBatteryLevel() async {
+    // Unique identifier for MethodChannel, url like
+    const platform = MethodChannel('course.flutter.dev/battery');
+    try {
+      // Future that will connect to the named method inside invokeMethod(), arguments gets passed like this [].
+      final batteryLevel = await platform.invokeMethod('getBatteryLevel');
+      setState(() {
+        _batteryLevel = batteryLevel;
+      });
+    } on PlatformException catch (error) {
+      setState(() {
+        _batteryLevel = null;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Goes to MainActivity.java file inside android/app/src/main/java/com/example/native_app
+    // Goes to ios/runner/AppDelegate.m
+    _getBatteryLevel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Native Code'),
+      ),
+      body: Center(
+        child: Text('Battery Level: $_batteryLevel'),
+      ),
+    );
+  }
+}
